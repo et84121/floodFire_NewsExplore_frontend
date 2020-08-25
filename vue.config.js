624522b82dab27d62740d62508ee1822030020ff
BIG_whale <f411024@gmail.com>
2020-08-25 15:02:45 +0800

    fix gh_page public path

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,65 @@
 module.exports = {
-  css: {
-    sourceMap: true
+  chainWebpack: config => {
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src"
+        };
+
+        return options;
+      });
+    config.module
+      .rule("vue")
+      .use("vue-loader")
+      .loader("vue-loader")
+      .tap(options => {
+        options.transformAssetUrls = {
+          img: "src",
+          image: "xlink:href",
+          "b-avatar": "src",
+          "b-img": "src",
+          "b-img-lazy": ["src", "blank-src"],
+          "b-card": "img-src",
+          "b-card-img": "src",
+          "b-card-img-lazy": ["src", "blank-src"],
+          "b-carousel-slide": "img-src",
+          "b-embed": "src"
+        };
+
+        return options;
+      });
   },
-  transpileDependencies: ["vue-echarts", "resize-detector"]
+  transpileDependencies: ["vue-echarts", "resize-detector"],
+  publicPath: process.env.PUBLIC_PATH || ".",
+  configureWebpack: {
+    devtool: "source-map"
+  },
+  pages: {
+    index: {
+      // page 的入口
+      entry: "src/main.js",
+      // 模板来源
+      template: "public/index.html",
+      // 在 dist/index.html 的输出
+      filename: "index.html",
+      // 当使用 title 选项时，
+      // template 中的 title 标签需要是 <title><%= htmlWebpackPlugin.options.title %></title>
+      title: process.env.VUE_APP_TITLE || "水火新聞資料查詢系統",
+      // 在这个页面中包含的块，默认情况下会包含
+      // 提取出来的通用 chunk 和 vendor chunk。
+      chunks: ["chunk-vendors", "chunk-common", "index"]
+    }
+  }
 };
